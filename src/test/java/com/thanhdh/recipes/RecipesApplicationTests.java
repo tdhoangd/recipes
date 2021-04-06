2829e327f5d5b45ba31c1fd1c7be08180acc4abd
Thanh Hoang <thanhhoangss@gmail.com>
2021-04-06 13:13:13 -0700

    first commit

---
+++ src/test/java/com/thanhdh/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhdh.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
